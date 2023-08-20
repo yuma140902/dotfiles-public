bbe4c950ffe6f9d2413c7f3ecb33e654e841249f
yuma14 <yuma140902@gmail.com>
2023-08-20 21:01:50 +0900

    pl: neogen

---
+++ neovim/nvim/lua/pl/neogen.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.config()
+  require 'neogen'.setup { snippet_engine = 'vsnip' }
+end
+
+return M
--- neovim/nvim/lua/rc/plugins.lua
+++ neovim/nvim/lua/rc/plugins.lua
@@ -196,9 +196,7 @@ local plugins = {
     -- ドキュメントコメントを生成してくれるやつ
     "danymat/neogen",
     dependencies = "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require 'neogen'.setup { snippet_engine = 'vsnip' }
-    end,
+    config = require 'pl/neogen'.config,
     cmd = 'Neogen'
     -- Uncomment next line if you want to follow only stable versions
     -- version = "*"
