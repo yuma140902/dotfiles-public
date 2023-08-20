f15f4fac5747f9c6b06bfe3b5bd6c5f1f80e55cb
yuma14 <yuma140902@gmail.com>
2023-08-20 21:03:53 +0900

    pl: tree-sitter

---
+++ neovim/nvim/lua/pl/tree-sitter.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+function M.config()
+  require 'nvim-treesitter.configs'.setup {
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+      -- Using this option may slow down your editor, and you may see some duplicate highlights.
+      -- Instead of true it can also be a list of languages
+      additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+      enable = true,
+      -- TODO: 活用方法がいまいちわからない
+      keymaps = {
+        init_selection = "tnn",     -- set to `false` to disable one of the mings
+        node_incremental = "grn",
+        scope_incremental = "grc",
+        node_decremental = "grm",
+      },
+    },
+    indent = {
+      enable = true
+    }
+  }
+end
+
+return M
--- neovim/nvim/lua/rc/plugins.lua
+++ neovim/nvim/lua/rc/plugins.lua
@@ -221,33 +221,7 @@ local plugins = {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     lazy = true,
-    config = function()
-      require 'nvim-treesitter.configs'.setup {
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
-        incremental_selection = {
-          enable = true,
-          -- TODO: 活用方法がいまいちわからない
-          keymaps = {
-            init_selection = "tnn", -- set to `false` to disable one of the mings
-            node_incremental = "grn",
-            scope_incremental = "grc",
-            node_decremental = "grm",
-          },
-        },
-        indent = {
-          enable = true
-        }
-      }
-    end,
+    config = require 'pl/tree-sitter'.config,
     cmd = { 'TSUpdate', 'TSEnable' },
     event = { 'BufNewFile', 'BufRead' }
   },
