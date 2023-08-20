eb3bae0ab3da3fea92c4cd16165fcc1e572416d7
yuma14 <yuma140902@gmail.com>
2023-08-20 21:14:39 +0900

    pl: auto-split-direction

--- neovim/nvim/lua/rc/plugins.lua
+++ neovim/nvim/lua/rc/plugins.lua
@@ -341,12 +341,7 @@ local plugins = {
     -- dir = '~/pj/nvim/auto-split-direction.nvim',
     branch = 'master',
     cmd = 'SplitAutoDirection',
-    config = function()
-      require 'auto-split-direction'.setup {
-        debug = false,
-        ratio = 3.0
-      }
-    end
+    config = require 'pl/auto-split-direction'.config,
   },
 
   -- }}}
