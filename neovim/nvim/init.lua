53365a7754fe162a2915c9bcec3f1ff43fcf0466
yuma14 <yuma140902@gmail.com>
2023-08-20 20:19:10 +0900

    add rc/plugins.lua

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,20 +1,6 @@
 require 'rc/disable_default_plugins'
 
--- lazy.nvim(プラグインマネージャ)を自動インストール
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require 'lazy'.setup 'plugins'
+require 'rc/plugins'
 
 require 'rc/base'
 
---
+++ neovim/nvim/lua/rc/plugins.lua
@@ -0,0 +1,17 @@
+-- プラグインに関する設定
+
+-- lazy.nvim(プラグインマネージャ)を自動インストール
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require 'lazy'.setup 'plugins'
