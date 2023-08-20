70605dc6d310f6c152225cf891acb31a17bb6436
yuma14 <yuma140902@gmail.com>
2023-08-20 20:20:34 +0900

    reorder require

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,10 +1,5 @@
 require 'rc/disable_default_plugins'
-
-require 'rc/plugins'
-
 require 'rc/base'
-
--- キーマップの設定
-require 'keymaps'.register_keymaps()
-
 require 'rc/gui'
+require 'rc/plugins'
+require 'keymaps'.register_keymaps()
