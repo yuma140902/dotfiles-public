b84da6f26508ecf0a0369a167de577bed8e10aff
yuma14 <yuma140902@gmail.com>
2023-08-20 21:24:27 +0900

    use lib.map in crates.lua

--- neovim/nvim/lua/pl/crates.lua
+++ neovim/nvim/lua/pl/crates.lua
@@ -14,31 +14,30 @@ function M.config()
   })
 
   -- キーマップの追加
-  -- TODO: 'rc/lib'.mapを使う
-  local crates = require('crates')
+  local crates = require 'crates'
+  local map = require 'rc.lib'.map
 
-  vim.keymap.set('n', '<space>ct', crates.toggle, { silent = true, desc = 'Toggle' })
-  vim.keymap.set('n', '<space>cr', crates.reload, { silent = true, desc = 'Reload' })
+  map('n', '<space>ct', crates.toggle, 'Toggle')
+  map('n', '<space>cr', crates.reload, 'Reload')
 
-  vim.keymap.set('n', '<space>cv', crates.show_versions_popup, { silent = true, desc = 'Show versions popup' })
-  vim.keymap.set('n', '<space>cf', crates.show_features_popup, { silent = true, desc = 'Show features popup' })
-  vim.keymap.set('n', '<space>cd', crates.show_dependencies_popup,
-    { silent = true, desc = 'Show dependencies popup' })
+  map('n', '<space>cv', crates.show_versions_popup, 'Show versions popup')
+  map('n', '<space>cf', crates.show_features_popup, 'Show features popup')
+  map('n', '<space>cd', crates.show_dependencies_popup, 'Show dependencies popup')
 
-  vim.keymap.set('n', '<space>cu', crates.update_crate, { silent = true, desc = 'Update crate' })
-  vim.keymap.set('v', '<space>cu', crates.update_crates, { silent = true, desc = 'Update crates' })
-  vim.keymap.set('n', '<space>ca', crates.update_all_crates, { silent = true, desc = 'Update ALL crates' })
-  vim.keymap.set('n', '<space>cU', crates.upgrade_crate, { silent = true, desc = 'Upgrade crate' })
-  vim.keymap.set('v', '<space>cU', crates.upgrade_crates, { silent = true, desc = 'Upgrade crates' })
-  vim.keymap.set('n', '<space>cA', crates.upgrade_all_crates, { silent = true, desc = 'Upgrade ALL crates' })
+  map('n', '<space>cu', crates.update_crate, 'Update crate')
+  map('v', '<space>cu', crates.update_crates, 'Update crates')
+  map('n', '<space>ca', crates.update_all_crates, 'Update ALL crates')
+  map('n', '<space>cU', crates.upgrade_crate, 'Upgrade crate')
+  map('v', '<space>cU', crates.upgrade_crates, 'Upgrade crates')
+  map('n', '<space>cA', crates.upgrade_all_crates, 'Upgrade ALL crates')
 
-  --vim.keymap.set('n', '<space>ce', crates.expand_plain_crate_to_inline_table, {silent = true})
+  --vim.keymap.set('n', '<space>ce', crates.expand_p desc =lain_crate_to_inline_table, {silent = true})
   --vim.keymap.set('n', '<space>cE', crates.extract_crate_into_table, {silent = true})
 
-  vim.keymap.set('n', '<space>cH', crates.open_homepage, { silent = true, desc = 'Open homepage' })
-  vim.keymap.set('n', '<space>cR', crates.open_repository, { silent = true, desc = 'Open repo' })
-  vim.keymap.set('n', '<space>cD', crates.open_documentation, { silent = true, desc = 'Open doc' })
-  vim.keymap.set('n', '<space>cC', crates.open_crates_io, { silent = true, desc = 'Open crates.io' })
+  map('n', '<space>cH', crates.open_homepage, 'Open homepage')
+  map('n', '<space>cR', crates.open_repository, 'Open repo')
+  map('n', '<space>cD', crates.open_documentation, 'Open doc')
+  map('n', '<space>cC', crates.open_crates_io, 'Open crates.io')
 end
 
 return M
