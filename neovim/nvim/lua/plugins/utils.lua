53ff11ffaf691c9b9b68bec0b7fbbc14b31d3961
yuma14 <yuma140902@gmail.com>
2023-08-20 17:26:46 +0900

    remove lexiv, add lexima

--- neovim/nvim/lua/plugins/utils.lua
+++ neovim/nvim/lua/plugins/utils.lua
@@ -11,7 +11,18 @@ return {
     event = { 'BufNewFile', 'BufRead' }
   },
   {
-    'mattn/vim-lexiv',
-    event = 'InsertEnter'
-  },
+    'cohama/lexima.vim',
+    event = 'InsertEnter',
+    init = function()
+      vim.g.lexima_enable_basic_rules = 1
+      vim.g.lexima_enable_newline_rules = 1
+      vim.g.lexima_enable_endwise_rules = 1
+    end,
+    config = function()
+      vim.cmd "call lexima#add_rule({'filetype': 'rust', 'char': '''', 'at': '&\\%#', 'input': ''''})"
+      vim.cmd "call lexima#add_rule({'filetype': 'latex', 'char': '$', 'input_after': '$'})"
+      vim.cmd "call lexima#add_rule({'filetype': 'latex', 'char': '$', 'at': '\\%#\\$', 'leave': 1})"
+      vim.cmd "call lexima#add_rule({'filetype': 'latex', 'char': '<BS>', 'at': '\\$\\%#\\$', 'delete': 1})"
+    end
+  }
 }
