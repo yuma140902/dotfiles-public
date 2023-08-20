4e281ea3b9487a7fa43bc54c2b9f3e40a2e114bc
yuma14 <yuma140902@gmail.com>
2023-08-20 20:54:38 +0900

    pl: telescope

---
+++ neovim/nvim/lua/pl/telescope.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+function M.config()
+  require 'telescope'.setup {
+    defaults = { file_ignore_patterns = { ".git" } },
+    extensions = {
+      command_palette = require 'command_palette'.table,
+      undo = {
+        side_by_side = true,
+        layout_strategy = 'vertical',
+        layout_config = {
+          preview_height = 0.8
+        }
+      }
+    },
+    pickers = {
+      buffers = {
+        mappings = {
+          i = {
+            ['<esc>'] = require 'telescope.actions'.close
+          },
+          n = {
+            ['<C-f>'] = require 'telescope.actions'.close
+          }
+        }
+      }
+    }
+  }
+  require 'telescope'.load_extension 'command_palette'
+  require 'telescope'.load_extension 'gh'
+  require 'telescope'.load_extension 'notify'
+  require 'telescope'.load_extension 'undo'
+end
+
+return M
--- neovim/nvim/lua/rc/plugins.lua
+++ neovim/nvim/lua/rc/plugins.lua
@@ -98,37 +98,7 @@ local plugins = {
       { 'nvim-telescope/telescope-github.nvim',   lazy = true },
       { 'debugloop/telescope-undo.nvim',          lazy = true },
     },
-    config = function()
-      require 'telescope'.setup {
-        defaults = { file_ignore_patterns = { ".git" } },
-        extensions = {
-          command_palette = require 'command_palette'.table,
-          undo = {
-            side_by_side = true,
-            layout_strategy = 'vertical',
-            layout_config = {
-              preview_height = 0.8
-            }
-          }
-        },
-        pickers = {
-          buffers = {
-            mappings = {
-              i = {
-                ['<esc>'] = require 'telescope.actions'.close
-              },
-              n = {
-                ['<C-f>'] = require 'telescope.actions'.close
-              }
-            }
-          }
-        }
-      }
-      require 'telescope'.load_extension 'command_palette'
-      require 'telescope'.load_extension 'gh'
-      require 'telescope'.load_extension 'notify'
-      require 'telescope'.load_extension 'undo'
-    end,
+    config = require 'pl/telescope'.config,
     cmd = 'Telescope'
   },
 
