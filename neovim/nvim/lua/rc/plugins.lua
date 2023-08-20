5a7e222b5faa74e8628c30c0913b1d24d7d05b55
yuma14 <yuma140902@gmail.com>
2023-08-20 21:16:35 +0900

    pl: crates

--- neovim/nvim/lua/rc/plugins.lua
+++ neovim/nvim/lua/rc/plugins.lua
@@ -140,47 +140,10 @@ local plugins = {
   { 'simrat39/rust-tools.nvim', lazy = true, ft = 'rust' }, -- LSPと連携してInline hintを表示するなど、いくつかの機能を追加する
   {
     'saecki/crates.nvim',
-    tag = 'v0.3.0',
+    tag = 'v0.3.0', -- TODO: バージョンを固定する必要があるのかわからない
     dependencies = { 'nvim-lua/plenary.nvim' },
     event = { "BufRead Cargo.toml" },
-    config = function()
-      require 'crates'.setup()
-
-      -- nvim-cmpソースの追加
-      vim.api.nvim_create_autocmd("BufRead", {
-        group = vim.api.nvim_create_augroup("CmpSourceCargo", { clear = true }),
-        pattern = "Cargo.toml",
-        callback = function()
-          require 'cmp'.setup.buffer({ sources = { { name = "crates" } } })
-        end,
-      })
-
-      -- キーマップの追加
-      local crates = require('crates')
-
-      vim.keymap.set('n', '<space>ct', crates.toggle, { silent = true, desc = 'Toggle' })
-      vim.keymap.set('n', '<space>cr', crates.reload, { silent = true, desc = 'Reload' })
-
-      vim.keymap.set('n', '<space>cv', crates.show_versions_popup, { silent = true, desc = 'Show versions popup' })
-      vim.keymap.set('n', '<space>cf', crates.show_features_popup, { silent = true, desc = 'Show features popup' })
-      vim.keymap.set('n', '<space>cd', crates.show_dependencies_popup,
-        { silent = true, desc = 'Show dependencies popup' })
-
-      vim.keymap.set('n', '<space>cu', crates.update_crate, { silent = true, desc = 'Update crate' })
-      vim.keymap.set('v', '<space>cu', crates.update_crates, { silent = true, desc = 'Update crates' })
-      vim.keymap.set('n', '<space>ca', crates.update_all_crates, { silent = true, desc = 'Update ALL crates' })
-      vim.keymap.set('n', '<space>cU', crates.upgrade_crate, { silent = true, desc = 'Upgrade crate' })
-      vim.keymap.set('v', '<space>cU', crates.upgrade_crates, { silent = true, desc = 'Upgrade crates' })
-      vim.keymap.set('n', '<space>cA', crates.upgrade_all_crates, { silent = true, desc = 'Upgrade ALL crates' })
-
-      --vim.keymap.set('n', '<space>ce', crates.expand_plain_crate_to_inline_table, {silent = true})
-      --vim.keymap.set('n', '<space>cE', crates.extract_crate_into_table, {silent = true})
-
-      vim.keymap.set('n', '<space>cH', crates.open_homepage, { silent = true, desc = 'Open homepage' })
-      vim.keymap.set('n', '<space>cR', crates.open_repository, { silent = true, desc = 'Open repo' })
-      vim.keymap.set('n', '<space>cD', crates.open_documentation, { silent = true, desc = 'Open doc' })
-      vim.keymap.set('n', '<space>cC', crates.open_crates_io, { silent = true, desc = 'Open crates.io' })
-    end
+    config = require 'pl.crates'.config,
   },
   {
     'mfussenegger/nvim-jdtls',
