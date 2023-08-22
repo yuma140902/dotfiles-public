49f162561a64ce6e099c48a7a2c1f7058d5f8f4f
yuma14 <yuma140902@gmail.com>
2023-08-22 13:14:55 +0900

    :quit -> :qa

--- neovim/nvim/lua/command_palette.lua
+++ neovim/nvim/lua/command_palette.lua
@@ -5,11 +5,11 @@ return {
     },
     { 'Session',
       { 'セッションを復元 (Restore session)', ':SessionRestore' },
-      { 'セッションを保存 (Save session)',    ':SessionSave' },
+      { 'セッションを保存 (Save session)', ':SessionSave' },
       { 'セッションを削除 (Delete session)',
         ':call feedkeys(":DisableAutoSave\\<cr>:SessionDelete\\<cr>")' },
       { 'セッションを削除して終了 (Delete session and quit)',
-        ':call feedkeys(":DisableAutoSave\\<cr>:SessionDelete\\<cr>:quit\\<cr>")' }
+        ':call feedkeys(":DisableAutoSave\\<cr>:SessionDelete\\<cr>:qa\\<cr>")' }
     }
   }
 }
