d2b43e74e59d50582cfa63a33bc5cdbc57e25f41
yuma14 <yuma140902@gmail.com>
2023-12-07 22:22:23 +0900

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,11 +9,17 @@ end
 --config.font = wezterm.font 'HackGen Console NF'
 --config.font = wezterm.font 'UDEV Gothic NF'
 config.font = wezterm.font 'PlemolJP Console NF'
-config.font_size = 10.0
+if wezterm.target_triplet == 'x86_64-pc-windows-msvc' then
+  config.font_size = 10.0
+else
+  config.font_size = 11.0
+end
 config.use_ime = true
 config.color_scheme = 'OneDark (base16)'
-config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
-config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+if wezterm.target_triplet == 'x86_64-pc-windows-msvc' then
+  config.default_prog = { 'C:\\Program Files\\PowerShell\\7\\pwsh.exe' }
+  config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+end
 config.window_padding = {
   left = 1,
   right = 0,
@@ -32,11 +38,19 @@ config.launch_menu = {
   {
     label = 'System status (btm)',
     args = { 'btm' }
-  },
-  {
-    label = 'cmd.exe',
-    args = { 'cmd' }
   }
 }
+if wezterm.target_triplet == 'x86_64-pc-windows-msvc' then
+  config.launch_menu = {
+    {
+      label = 'System status (btm)',
+      args = { 'btm' }
+    },
+    {
+      label = 'cmd.exe',
+      args = { 'cmd' }
+    }
+  }
+end
 
 return config
