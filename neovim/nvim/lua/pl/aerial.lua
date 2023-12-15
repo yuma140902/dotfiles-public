88d319d75c00eff6e5dd3ad94812c1cc9fc76551
yuma14 <yuma140902@gmail.com>
2023-12-16 04:42:35 +0900

    aerialのウィンドウはセッションに保存しない

--- neovim/nvim/lua/pl/aerial.lua
+++ neovim/nvim/lua/pl/aerial.lua
@@ -1,6 +1,15 @@
 local M = {}
 
 function M.config()
+  local g = vim.api.nvim_create_augroup("aerial_quit", {})
+  vim.api.nvim_create_autocmd({ "User" }, {
+    pattern = "PersistedSavePre",
+    group = g,
+    callback = function()
+      require 'aerial'.close_all()
+    end,
+  })
+
   require 'aerial'.setup {
     backends = { 'lsp', 'treesitter', 'markdown', 'man' },
   }
