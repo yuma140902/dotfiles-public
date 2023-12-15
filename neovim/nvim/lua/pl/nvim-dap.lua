852b2a6d8b115a39b00e838cbb68bb353bbe931a
yuma14 <yuma140902@gmail.com>
2023-12-16 04:38:42 +0900

    dapの設定

--- neovim/nvim/lua/pl/nvim-dap.lua
+++ neovim/nvim/lua/pl/nvim-dap.lua
@@ -1,11 +1,13 @@
 local M = {}
 
 function M.config()
+  local exe_suffix = vim.fn.has('win32') == 1 and '.exe' or ''
+
   require 'dap'.adapters.codelldb = {
     type = 'server',
     port = '${port}',
     executable = {
-      command = vim.fn.stdpath 'data' .. '/mason/packages/codelldb/extension/adapter/codelldb.exe',
+      command = vim.fn.stdpath 'data' .. '/mason/packages/codelldb/extension/adapter/codelldb' .. exe_suffix,
       args = { '--port', '${port}' }
     }
   }
@@ -13,7 +15,34 @@ function M.config()
   require 'dap'.configurations = {
     rust = {
       {
-        name = 'Debug a generic Rust program',
+        name = '[CodeLLDB] Debug generic Rust program',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/target/debug/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {}
+      }
+    },
+
+    cpp = {
+      {
+        name = '[CodeLLDB] Debug generic C++ program',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {}
+      }
+
+    c = {
+      {
+        name = '[CodeLLDB] Debug generic C program',
         type = 'codelldb',
         request = 'launch',
         program = function()
