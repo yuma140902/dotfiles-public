938bf248912f3ddc9ede50afe3113b587bf927b2
yuma14 <yuma140902@gmail.com>
2024-01-22 15:32:47 +0900

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
 
-local config = {}
+local config = wezterm.config_builder()
 
 if wezterm.config_builder then
   config = wezterm.config_builder()
@@ -27,8 +27,44 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
+
+local function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
+  end
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config_, hover, max_width)
+    local title = tab_title(tab)
+    local bg = '#282c34'
+    local fg = 'white'
+    if tab.is_active then
+      bg = '#98c379'
+      fg = '#282c34'
+    elseif hover then
+      bg = '#31353f'
+    end
+    if tab.is_active then
+      return {
+        { Background = { Color = bg } },
+        { Foreground = { Color = fg } },
+        { Text = ' ' .. title .. ' ' },
+      }
+    end
+    return title
+  end
+)
+
+
 config.window_background_opacity = 1.0
--- config.win32_system_backdrop = 'Mica'
+-- config.win32_system_backdrop = 'Acrylic'
+
 config.default_cursor_style = 'SteadyBlock'
 config.cursor_blink_rate = 800
 config.cursor_blink_ease_in = 'Constant'
