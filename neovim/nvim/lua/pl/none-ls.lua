6cd9d9b004c917de15a27d4eb5eeb09bdf5d2dc6
yuma14 <yuma140902@gmail.com>
2024-03-22 22:21:55 +0900

    feat(neovim): remove ESLint from none-ls

--- neovim/nvim/lua/pl/none-ls.lua
+++ neovim/nvim/lua/pl/none-ls.lua
@@ -4,7 +4,6 @@ function M.config()
   local null_ls = require 'null-ls'
   null_ls.setup {
     sources = {
-      null_ls.builtins.code_actions.eslint,
       null_ls.builtins.formatting.prettier,
       null_ls.builtins.formatting.clang_format
     }
