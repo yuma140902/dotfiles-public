44c9ed5c483269ae2118c1c755f381cee78717cc
yuma14 <yuma140902@gmail.com>
2024-03-23 01:23:20 +0900

    feat(neovim): change neovide settings

--- neovim/nvim/lua/rc/gui.lua
+++ neovim/nvim/lua/rc/gui.lua
@@ -1,7 +1,7 @@
 -- GUIに関する設定
 
-vim.o.guifont = 'HackGen Console NF:h10'
+vim.o.guifont = 'PlemolJP Console NF:h10'
 vim.o.autochdir = false
 if vim.g.neovide then
-  vim.g.neovide_remember_window_size = false
+  vim.g.neovide_remember_window_size = true
 end
