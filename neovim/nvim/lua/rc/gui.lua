602e241e92b790d5c57b2fec1add9fdb239c2eb3
yuma14 <yuma140902@gmail.com>
2024-04-19 15:40:02 +0900

    fix(neovim): neovide on macOS

--- neovim/nvim/lua/rc/gui.lua
+++ neovim/nvim/lua/rc/gui.lua
@@ -1,6 +1,10 @@
 -- GUIに関する設定
 
-vim.o.guifont = 'PlemolJP Console NF:h10'
+if vim.fn.has 'win32' == 1 then
+  vim.o.guifont = 'PlemolJP Console NF:h10'
+else
+  vim.o.guifont = 'PlemolJP Console NF:h14'
+end
 vim.o.autochdir = false
 if vim.g.neovide then
   vim.g.neovide_remember_window_size = true
