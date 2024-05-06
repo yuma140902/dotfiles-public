de9325b7082dfe19dd46f60d814dc9c40cc3f6df
yuma14 <yuma140902@gmail.com>
2024-05-06 18:56:03 +0900

    feat(neovim): change ambiwidth

--- neovim/nvim/lua/pl/ambiwidth.lua
+++ neovim/nvim/lua/pl/ambiwidth.lua
@@ -3,7 +3,10 @@ local M = {}
 function M.init()
   vim.g.ambiwidth_add_list = {
     { 0xe640,  0xe640,  2 },
-    { 0xf073a, 0xf073a, 2 }
+    { 0xea7b,  0xea83,  2 },
+    { 0xea88,  0xea96,  2 },
+    { 0xeb5b,  0xeb66,  2 },
+    { 0xf073a, 0xf073a, 2 },
   }
 end
 
