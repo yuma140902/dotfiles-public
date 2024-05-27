35a94c9a8c68b483921b6ac6531970ffcfd541b7
yuma14 <yuma140902@gmail.com>
2024-05-27 13:57:57 +0900

    feat(neovim): enable crate name completion

--- neovim/nvim/lua/pl/crates.lua
+++ neovim/nvim/lua/pl/crates.lua
@@ -6,6 +6,11 @@ function M.config()
       cmp = {
         enabled = true,
       },
+      crates = {
+        enabled = true,  -- disabled by default
+        max_results = 8, -- The maximum number of search results to display
+        min_chars = 3,   -- The minimum number of characters to type before completions begin appearing
+      }
     },
     lsp = {
       enabled = true,
