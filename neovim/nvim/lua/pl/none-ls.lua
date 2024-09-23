13d1d986eede2c759e09b669dfe25897633abce1
yuma14 <yuma140902@gmail.com>
2024-09-23 23:46:48 +0900

    feat(nvim): use biome instead of prettier

--- neovim/nvim/lua/pl/none-ls.lua
+++ neovim/nvim/lua/pl/none-ls.lua
@@ -4,7 +4,7 @@ function M.config()
   local null_ls = require 'null-ls'
   null_ls.setup {
     sources = {
-      null_ls.builtins.formatting.prettier,
+      null_ls.builtins.formatting.biome,
       null_ls.builtins.formatting.clang_format,
       null_ls.builtins.formatting.dart_format,
       null_ls.builtins.diagnostics.textlint.with {
