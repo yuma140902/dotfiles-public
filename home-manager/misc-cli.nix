55bdd18b28506a95f3acd330b6c1f4ef2d2a7f72
yuma14 <yuma140902@gmail.com>
2025-02-21 15:02:41 +0000

    feat: add htop and btop

--- home-manager/misc-cli.nix
+++ home-manager/misc-cli.nix
@@ -8,4 +8,9 @@
 	programs.bat.enable = true;
 	programs.ripgrep.enable = true;
 	programs.less.enable = true;
+	programs.htop.enable = true;
+	programs.btop.enable = true;
+	programs.btop.settings = {
+		color_theme = "onedark";
+	};
 }
