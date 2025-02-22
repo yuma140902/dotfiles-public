ba94ff0755ee4af1db59ea6595680fa38cb1b28b
yuma14 <yuma140902@gmail.com>
2025-02-22 19:18:58 +0000

    feat(zsh): place file with nix

--- zsh/default.nix
+++ zsh/default.nix
@@ -1,37 +1,12 @@
 { pkgs, ... }:
 
 {
-	programs.zsh = {
-		enable = true;
-		enableCompletion = true;
-		antidote = {
-			enable = true;
-			plugins = [
-				"zsh-users/zsh-syntax-highlighting"
-				"zsh-users/zsh-autosuggestions"
-				"zsh-users/zsh-completions"
-				"sindresorhus/pure"
-				"olets/zsh-abbr"
-				"chisui/zsh-nix-shell"
-			];
-		};
-		initExtraFirst = ''
-			setopt EXTENDED_GLOB NOMATCH NOTIFY
-			unsetopt AUTO_CD BEEP
-			unsetopt CLOBBER
+	home.packages = [
+		pkgs.zsh
+	];
 
-			HISTFILE=~/.zsh_history
-			HISTSIZE=1000000
-			SAVEHIST=1000000
-			#HISTORY_IGNORE="(ls|cd|nvim|vim)"
-			setopt HIST_IGNORE_ALL_DUPS
-			setopt HIST_IGNORE_DUPS
-			setopt SHARE_HISTORY
-			setopt EXTENDED_HISTORY
-			setopt HIST_SAVE_NO_DUPS
-
-			bindkey -e
-		'';
+	home.file = {
+		"./.zshrc".source = ./.zshrc;
 	};
 
 	xdg.configFile = {
