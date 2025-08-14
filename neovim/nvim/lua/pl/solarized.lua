85dc7598b5848535747ffe7bad8c98d6195406b7
yuma14 <yuma140902@gmail.com>
2025-08-14 16:14:10 +0900

    feat(neovim): システムのテーマに合わせてカラースキームを決める

--- neovim/nvim/lua/pl/solarized.lua
+++ neovim/nvim/lua/pl/solarized.lua
@@ -1,8 +1,40 @@
 local M = {}
 
+local function system_is_dark_mode()
+  local is_dark_mode = false
+  local os_name = vim.loop.os_uname().sysname
+
+  -- OSごとにダークモードかどうかを判定
+  if os_name == 'Darwin' then -- macOS
+    -- "AppleInterfaceStyle" キーが存在すればダークモード
+    local handle = io.popen('defaults read -g AppleInterfaceStyle 2>/dev/null')
+    if handle then
+      local result = handle:read('*a')
+      handle:close()
+      return result:match('Dark') ~= nil
+    end
+  elseif os_name == 'Windows_NT' then -- Windows
+    -- レジストリの値が 0 ならダークモード
+    local handle = io.popen(
+      'reg query "HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize" /v "AppsUseLightTheme"')
+    if handle then
+      local result = handle:read('*a')
+      handle:close()
+      return result:match('0x0') ~= nil
+    end
+  end
+
+  local current_hour = tonumber(os.date("%H"))
+  return current_hour >= 18 or current_hour < 6
+end
+
 function M.config()
   vim.o.termguicolors = true
-  vim.o.background = 'light'
+  if system_is_dark_mode() then
+    vim.o.background = 'dark'
+  else
+    vim.o.background = 'light'
+  end
   require('solarized').setup {
     error_lens = {
       text = true,
