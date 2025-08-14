0c6d0e9cc9cfa8c686abeaaf9833e905ea11832e
yuma14 <yuma140902@gmail.com>
2025-08-14 16:00:55 +0900

    feat(neovim): setup solalized light

--- neovim/nvim/lua/pl/solarized.lua
+++ neovim/nvim/lua/pl/solarized.lua
@@ -3,7 +3,12 @@ local M = {}
 function M.config()
   vim.o.termguicolors = true
   vim.o.background = 'light'
-  require('solarized').setup {}
+  require('solarized').setup {
+    error_lens = {
+      text = true,
+      symbol = true,
+    }
+  }
   vim.cmd.colorscheme 'solarized'
 end
 
