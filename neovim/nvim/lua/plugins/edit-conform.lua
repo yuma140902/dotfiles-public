ea0610b80a4c8b8eb17d29d8c5dd488af2948be9
yuma14 <yuma14@sslab.ics.keio.ac.jp>
2026-06-08 02:39:54 +0900

    feat(neovim): フォーマットの際に biome が利用可能なら優先する

--- neovim/nvim/lua/plugins/edit-conform.lua
+++ neovim/nvim/lua/plugins/edit-conform.lua
@@ -12,7 +12,17 @@ return {
 
   config = function()
     require 'conform'.setup {
-      formatters_by_ft = {},
+      formatters_by_ft = {
+        javascript = { "biome" },
+        javascriptreact = { "biome" },
+        typescript = { "biome" },
+        typescriptreact = { "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
+        css = { "biome" },
+        graphql = { "biome" },
+        html = { "biome" },
+      },
       default_format_opts = {
         lsp_format = 'fallback'
       },
@@ -23,17 +33,6 @@ return {
         return {
           lsp_format = 'fallback',
           async = true,
-          filter = function(client_)
-            -- bufnrとclient_をもとに、各LSPサーバでフォーマットを行うかどうか決定する
-            if client_.name == 'ts_ls' or client_.name == 'null-ls' then
-              -- biomeがアタッチされているとき、ts_lsやnull-lsではフォーマットしない
-              local biome_attached = #(vim.lsp.get_clients({ bufnr = bufnr, name = 'biome' })) ~= 0
-              if biome_attached then
-                return false
-              end
-            end
-            return true
-          end,
         }
       end,
       notify_no_formatters = true,
